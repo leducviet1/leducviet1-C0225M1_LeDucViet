03e9c3b5a56c9cc527873da731ec3373bc6b9519
LeDucViet <ldviettb04@gmail.com>
2025-03-23 23:44:28 +0700

    THêm bài 9 và thực hành chuột

---
+++ bai9_cau_truc_dieu_kien_2/ly_thuyet/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.getElementById('myButton').addEventListener('click', function() {
+    var message = "Hello, world!";
+    console.log(message);
+});
+function calculateSum(a, b) {
+    return a + b;
+}
+console.log(calculateSum(5, 10));
